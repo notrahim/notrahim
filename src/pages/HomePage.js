2dde7614f24a83da1127dd6039102c894db8d3b3
Nicolas <casteranicolas.contact@gmail.com>
2022-03-12 15:56:29 +0100

    Adding of "HomePage.js" and creation of router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <header></header>
+            <main>
+                <h1>Maison TeraBois</h1>
+                <p>“Ex commodi voluptatem eos rerum quasi et quis tenetur et iure voluptatem ea quis dolorem. Ut laudantium optio et odit ipsam id reprehenderit voluptatem. Et internos rerum ut perferendis reprehenderit qui beatae fuga a aspernatur excepturi.”</p>
+            </main>
+        </>
+    );
+};
+
+export default HomePage;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <>
+            <header></header>
+            <main>
+                <h1>Page 404</h1>
+            </main>
+        </>
+    );
+};
+
+export default NotFound;
