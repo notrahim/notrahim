eb8e907b47813478f8c14b3782c9ba84e2b6578e
Nicolas <casteranicolas.contact@gmail.com>
2022-03-16 12:51:51 +0100

    Modification of global style

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import LandingPageNavigation from '../component/LandingPageNavigation';
 //TODO Creer une modal pour qu'a l'ouverture de la page, la partie main soit entièrement visible 
 //BUG Je pense que la solution est d'iterer la navbar au scroll
@@ -11,7 +12,13 @@ const LandingPage = () => {
             <main className="landingPage"> 
                 <div className="globalContainer">
                     <div className="leftContainer">
-                        <div className="container"></div>
+                        <div className="container">
+                            <div className="whereAreMe">
+                                <NavLink to="/">Terabois</NavLink>
+                                <img src="./img/SVG/chevron-right-black.png" alt="chevron right"/>
+                                <p>Isolation</p>
+                            </div>
+                        </div>
                     </div>
                     <div className="rightContainer"></div>
                 </div>
