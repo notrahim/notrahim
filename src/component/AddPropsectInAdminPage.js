1ac957fc08e44eb0df2265c516962039a0ab350a
Nicolas <casteranicolas.contact@gmail.com>
2022-05-11 10:06:47 +0200

    Create AddPropsectInAdminPage.js

---
+++ src/component/AddPropsectInAdminPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPropsectInAdminPage = () => {
+    return (
+        <div className='addPropsectInAdminPage'>
+            
+        </div>
+    );
+};
+
+export default AddPropsectInAdminPage;
