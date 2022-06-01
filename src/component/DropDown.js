117ef1589843e9fc7bf835311a2f7bd41d995517
Nicolas <casteranicolas.contact@gmail.com>
2022-06-01 14:32:57 +0200

    Update DropDown.js

--- src/component/DropDown.js
+++ src/component/DropDown.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 //TODO changer l'image du dropdown par fontawesome
 
 const DropDown = (props) => {
-    
-    /* It's a hook that allows us to use state in a functional component. */
-    const [openBtn, setOpenBtn] = useState(false);
 
     /* It's destructuring the props. */
     const question = props.question
@@ -19,12 +16,21 @@ const DropDown = (props) => {
     /* It's destructuring the props. */
     const classValue = props.class
 
+    const callback = props.callback;
+
+    const showReturn = props.return;
+
+    const handleclick = ()=>{
+        callback(!showReturn)
+        props.arg()
+    }
+
     /* It's a ternary operator. It's a conditional operator that assigns a value to a variable based on
     some condition. */
-    if(!openBtn) {
+    if(!showReturn) {
         return (
             <div className={classValue === undefined ? "dropDownContainer": "dropDownContainer " + classValue}>
-                <div onClick={()=>setOpenBtn(!openBtn)} className="dropDown">
+                <div onClick={handleclick} className="dropDown">
                     <p>{question}</p>
                     <img src="../img/SVG/1x/chevronBottom.png" alt="chevron bottom" />
                 </div>
@@ -37,7 +43,7 @@ const DropDown = (props) => {
         some condition. */
     return (
         <div className={classValue === undefined ? "dropDownContainer": "dropDownContainer " + classValue}>
-            <div onClick={()=>setOpenBtn(!openBtn)} className="dropDown">
+            <div onClick={handleclick} className="dropDown">
                     <p>{question}</p>
                 <img src="../img/SVG/1x/chevron.png" alt="chevron top" className='top' />
             </div>
